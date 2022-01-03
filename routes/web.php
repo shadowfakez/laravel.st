aa6f0c5cd202b0536d5464949911a2becc2ed45c
shadowfakez <iamshadowfake@gmail.com>
2022-01-03 20:51:54 +0200

    Laravel Breeze added (auth)

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,9 @@ Route::group(['prefix' => 'users'], function() {
 });
 
 Route::resource('/task', TaskController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
