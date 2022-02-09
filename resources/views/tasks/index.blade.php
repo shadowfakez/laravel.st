28e7ef37a37911344216b40d52721ca3ff61fc9e
shadowfakez <iamshadowfake@gmail.com>
2022-02-09 22:18:29 +0200

    tasks design changed

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,58 +2,70 @@
 
 @section('content')
 
+    <div class="page-header mb-3">
+        <h3 class="text-center">Tasks</h3>
+    </div>
 
-        <div class="page-header mb-3">
-            <h3 class="text-center">Tasks</h3>
-        </div>
+    <div class="p-3 m-3 text-center">
+        <button class="btn btn-success"><a class="btn btn-success p-0 m-0" href="{{ route('task.create') }}">Create new task</a></button>
+    </div>
 
-        <div class="p-3 m-3">
-            <button class="btn btn-success"><a class="btn btn-success p-0 m-0" href="{{ route('task.create') }}">Create new task</a></button>
-        </div>
-        <table class="table table-dark">
-            <thead>
-            <tr>
-                <th scope="col">Labels</th>
-                <th scope="col">Id</th>
-                <th scope="col">Creator</th>
-                <th scope="col">Title</th>
-                <th scope="col">Content</th>
-                <th scope="col">Status</th>
-                <th scope="col">Edit</th>
-                <th scope="col">Delete</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($tasks as $task)
-                <tr>
-                    <th scope="row">
+    <div class="row justify-content-md-center">
+    @foreach($tasks as $task)
+        <div class="col-md-8 mb-4">
+            <div class="card gradient-card text-white bg-dark">
+                <div class="card-header border-bottom border-light">
+
+                    <div class="text-right border-bottom border-light rounded-top">
+                        <p class="text-uppercase text-center m-2">{{ $task->status->name }}</p>
+                    </div>
+
+                    <div class="align-self-center p-3">
+                        <p class="justify-center card-title fs-3">{{ $task->title }}</p>
+                    </div>
+
+                </div>
+
+                <div class="card-body white border-bottom border-light">
+                    <div class="ml-auto mr-4">
+                        <p class="m-4 fs-5">{{ $task->content }}</p>
+                    </div>
+                </div>
+
+                <div class="card-body border-bottom border-light">
+
                         @foreach($task->labels as $label)
                             <span class="badge bg-{{$label->color}}">{{$label->name}}</span>
                         @endforeach
-                    </th>
-                    <td>{{ $task->id }}</td>
-                    <td>{{ $task->user->name }}</td>
-                    <td>{{ $task->title }}</td>
-                    <td>{{ $task->content }}</td>
-                    <td>{{ $task->status->name }}</td>
-                    <td>
-                        <button class="btn btn-info" type="submit">
+
+                </div>
+
+                <div class="card-body white text-end">
+                    <div class="border-bottom border-light">
+                        <h4 class="fs-5">Created by {{ $task->user->name }}</h4>
+                    </div>
+                </div>
+
+                <div class="card-body row">
+                    <div class="col d-grid gap-2">
+                        <button class="btn btn-outline-info btn-sm" type="submit">
                             <a href="{{ route('task.edit', ['task' => $task->id]) }}" class="text-white m-0 p-0">
                                 Edit
                             </a>
                         </button>
-                    </td>
-                    <td>
-                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                    </div>
+                    <div class="col">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST" class=" d-grid gap-2">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-danger" type="submit" onclick="return confirm('Подтвердите удаление')">
+                            <button class="btn btn-outline-danger btn-sm" type="submit" onclick="return confirm('Подтвердите удаление')">
                                 Delete
                             </button>
                         </form>
-                    </td>
-                </tr>
-            @endforeach()
-            </tbody>
-        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 @endsection
